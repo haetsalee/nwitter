9a0fe491c4c4b0324c5cac39bc15d029798daf7b
이햇살 <leeee99@hanmail.net>
2022-12-20 00:51:05 +0900

    1219

--- src/fbase.js
+++ src/fbase.js
@@ -1,10 +1,11 @@
 import firebase from "firebase/compat/app"
 import "firebase/compat/auth";
+import "firebase/compat/firestore";
 
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_API_KEY,
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectID: process.env.REACT_APP_PROJECT_ID,
+  projectId: process.env.REACT_APP_PROJECT_ID,
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messageingSenderId: process.env.REACT_APP_MESSAGIN_ID,
   appId: process.env.REACT_APP_APP_ID
@@ -15,5 +16,6 @@ firebase.initializeApp(firebaseConfig);
 export const firebaseInstance = firebase
 
 export const authService = firebase.auth();
+export const dbService = firebase.firestore();
 
  
