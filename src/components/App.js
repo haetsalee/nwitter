473c9ee4243a2121e789d156188343a20732c0f8
이햇살 <leeee99@hanmail.net>
2022-12-21 22:18:11 +0900

    1221

--- src/components/App.js
+++ src/components/App.js
@@ -9,20 +9,34 @@ function App() {
     authService.onAuthStateChanged((user) => {
       if (user) {
         setUserObj(user)
+        setUserObj({
+          displayName: user.displayName,
+          uid: user.uid,
+          updateProfile: (args) => user.updateProfile(args),
+        })
       } 
       setInit(true)
     })
   }, [])
+  const refreshUser = () => {
+    const user = authService.currentUser
+    setUserObj({
+      displayName: user.displayName,
+      uid: user.uid,
+      updateProfile: (args) => user.updateProfile(args),
+    })
+  }
   return (
     <>
       {init ? (
-        <AppRouter isLoggedIn={Boolean(userObj)} userObj={userObj} />
+        <AppRouter
+          refreshUser={refreshUser}
+          isLoggedIn={Boolean(userObj)}
+          userObj={userObj}
+        />
       ) : (
         "Initializing..."
       )}
-      <footer>
-        &copy; { new Date().getFullYear() } Nwitter
-      </footer>
     </>
   )
 }
